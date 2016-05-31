bab2687a7436530203d04915061a17c5ddb48eee
Rebecca Hom <rhom0505@gmail.com>
2016-05-31 16:18:48 -0700

    fixed up PHP setup

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -9,5 +9,4 @@ sudo apt --installed list | grep mysql-server &> /dev/null && echo "MySQL server
 sudo apt --installed list php5-mysql &> /dev/null && echo "MySQL PHP has already been installed..." || (echo "Installing MySQL PHP now..." && sudo apt-get -y install php5-mysql &> /dev/null)
 
 # Checks if PHP is installed
-sudo apt --installed list | grep php &> /dev/null && echo "PHP is already installed..." || (echo "Installing PHP now..." && (sudo apt-get -y install php &> /dev/null || sudo apt-get -y php5 &> /dev/null)
-                             
+sudo apt --installed list | grep php &> /dev/null && echo "PHP is already installed..." || (echo "Installing PHP now..." && (sudo apt-get -y install php &> /dev/null || sudo apt-get -y php5 &> /dev/null))
