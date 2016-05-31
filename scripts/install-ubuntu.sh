ff0d975262496674c957e00d01a62889e8f03b75
Rebecca Hom <rhom0505@gmail.com>
2016-05-31 14:39:17 -0700

    added in scripts and sql directories

---
+++ scripts/install-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Installs LAMP on to an Ubuntu server
+# Checks if Apache is installed
+sudo apt --installed list | grep apache2 &> /dev/null && echo "Apache is already installed..." || (echo "Installing Apache now..." && sudo apt-get -y install apache2 &> /dev/null)
+
+# Checks if MySQL is installed
+sudo apt --installed list | grep mysql-server &> /dev/null && echo "MySQL server is already installed..." || (echo "Installing MySQL server now..." && sudo apt-get -y install mysql-server)
+
+sudo apt --installed list php5-mysql &> /dev/null && echo "MySQL PHP has already been installed..." || (echo "Installing MySQL PHP now..." && sudo apt-get -y install php5-mysql)
+
+# Checks if PHP is installed
+sudo apt --installed list | grep php5 &> /dev/null && echo "PHP is already installed..." || (echo "Installing PHP now..." && sudo apt-get -y install php5)
+                             
--- ubuntu-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Installs LAMP on to an Ubuntu server
-sudo apt-get install apache2 mysql-server php libapache2-mod-php php-mcrypt php-mysql php-cli
-
-# Turns on Apache port
-sudo ufw allow in "Apache Full"
-
-# Does secure installation for MySQL (set password validator to MEDIUM and yes to everything else)
-sudo mysql_secure_installation
