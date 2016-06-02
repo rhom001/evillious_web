f59deddb2da753fbd567a8ba161735b622a92539
Rebecca Hom <rhom0505@gmail.com>
2016-06-01 17:29:15 -0700

    corrected script

--- scripts/database-mothy-update.sh
+++ scripts/database-mothy-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Assumes that MySQL has already been installed and that root has no password.
-sudo mysql --user=root < ../sql/mothy/database-create.sql
-sudo mysql --user=root < ../sql/mothy/database-update.sql
+sudo mysql < ../sql/mothy/database-create.sql
+sudo mysql < ../sql/mothy/database-update.sql
