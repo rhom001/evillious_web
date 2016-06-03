68a1974c920a56a40e1af29ffaa66c6908f8204a
Rebecca Hom <rhom0505@gmail.com>
2016-06-02 22:10:58 -0700

    verified file paths

--- sql/mothy/database-backup.sql
+++ sql/mothy/database-backup.sql
@@ -6,35 +6,35 @@ USE mothy;
 SELECT * FROM Users INTO OUTFILE '/var/lib/mysql-files/User.csv'
     FIELDS TERMINATED BY ',';
 
-SELECT * FROM Song INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Song.csv'
+SELECT * FROM Song INTO OUTFILE '/var/lib/mysql-files/Song.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Vocaloid INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Vocaloid.csv'
+SELECT * FROM Vocaloid INTO OUTFILE '/var/lib/mysql-files/Vocaloid.csv'
     FIELDS TERMINATED BY ',';
     
 SELECT * FROM Persona INTO OUTFILE '/var/lib/mysql-files/Character.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Appear INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Role.csv'
+SELECT * FROM Appear INTO OUTFILE '/var/lib/mysql-files/Role.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Album INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Album.csv'
+SELECT * FROM Album INTO OUTFILE '/var/lib/mysql-files/Album.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Track INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Track.csv'
+SELECT * FROM Track INTO OUTFILE '/var/lib/mysql-files/Track.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Series INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Series.csv'
+SELECT * FROM Series INTO OUTFILE '/var/lib/mysql-files/Series.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Part INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Part.csv'
+SELECT * FROM Part INTO OUTFILE '/var/lib/mysql-files/Part.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Canon INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Canon.csv'
+SELECT * FROM Canon INTO OUTFILE '/var/lib/mysql-files/Canon.csv'
     FIELDS TERMINATED BY ',';
     
-SELECT * FROM Video INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Video.csv'
+SELECT * FROM Video INTO OUTFILE '/var/lib/mysql-files/Video.csv'
     FIELDS TERMINATED BY ',';
 
-SELECT * FROM Artist INTO OUTFILE '/var/lib/mysql-files/ec-web/data-mothy/Artist.csv'
+SELECT * FROM Artist INTO OUTFILE '/var/lib/mysql-files/Artist.csv'
     FIELDS TERMINATED BY ',';
