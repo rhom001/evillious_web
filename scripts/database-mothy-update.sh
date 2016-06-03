8220c9f822010e7a27d8e396737abd1a43ece4f2
Rebecca Hom <rhom0505@gmail.com>
2016-06-02 21:59:10 -0700

    changed update configuration

--- scripts/database-mothy-update.sh
+++ scripts/database-mothy-update.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Assumes that MySQL has already been installed and that root has no password.
+# Creates the database.
 sudo mysql < sql/mothy/database-create.sql
-sudo mysql < sql/mothy/database-update.sql
+
+# Moves to the correct directory for the local directory.
+cd ~/evillious_web/sql/mothy
+sudo mysql < ~/evillioud_web/sql/mothy/database-update.sql
