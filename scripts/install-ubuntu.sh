987740930da5e782e681c9f26f872f33f122c8a6
Rebecca Hom <rhom0505@gmail.com>
2016-06-03 20:46:43 -0700

    added in command to install PHP7 versions

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -6,7 +6,7 @@ sudo dpkg --list | grep apache2 &> /dev/null && echo "Apache is already installe
 # Checks if MySQL is installed
 sudo dpkg --list | grep mysql-server &> /dev/null && echo "MySQL server is already installed..." || (echo "Installing MySQL server now..." && sudo apt-get -y install mysql-server &>/dev/null)
 
-sudo dpkg --list php5-mysql &> /dev/null && echo "MySQL PHP has already been installed..." || (echo "Installing MySQL PHP now..." && sudo apt-get -y install php5-mysql &> /dev/null)
+sudo dpkg --list php5-mysql &> /dev/null && echo "MySQL PHP has already been installed..." || (echo "Installing MySQL PHP now..." && (sudo apt-get -y install php5-mysql &> /dev/null || sudo apt-get -y install php-mysql))
 
 # Checks if PHP is installed
 sudo dpkg --list | grep php &> /dev/null && echo "PHP is already installed..." || (echo "Installing PHP now..." && (sudo apt-get -y install php &> /dev/null || sudo apt-get -y php5 &> /dev/null))
